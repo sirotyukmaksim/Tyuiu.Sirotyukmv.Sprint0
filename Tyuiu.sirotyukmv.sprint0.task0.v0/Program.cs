da15064c383f7bd3d59f677fdbfcec6fa6cfda79
vladimir <vladimir@DESKTOP-HU6RCIE>
2026-09-07 17:53:58 +0500

    Add project files.

---
+++ Tyuiu.sirotyukmv.sprint0.task0.v0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.sirotyukmv.sprint0.task0.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
