62a94bf4090b4584b8288ebb435c861865c75453
максим сиротюк <Tyuiu.sirotyukmv@outlook.com>
2026-09-07 19:58:43 +0500

    Task2

---
+++ Tyuiu.Sirotyukmv.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.Sirotyukmv.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.Sirotyukmv.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
