a9a1a7c2047a126f46e38d7dd4d86f100991ab42
максим сиротюк <Tyuiu.sirotyukmv@outlook.com>
2026-09-08 14:41:08 +0500

    Task5

---
+++ Tyuiu.Sirotyukmv.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.Sirotyukmv.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.Sirotyukmv.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
